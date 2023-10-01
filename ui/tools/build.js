7810a93626f9a4718c4a4b6f2036d2c591628521
Chris Reeder <reederc42@gmail.com>
2023-10-01 06:26:50 +0000

    refactors build steps

---
+++ ui/tools/build.js
@@ -0,0 +1,28 @@
+const {execSync} = require('node:child_process');
+const fs = require('fs');
+const outdir = 'dist';
+const esbuildBin = './node_modules/.bin/esbuild';
+
+const baseOptions = [
+    '--bundle',
+    '--outfile=dist/index.js',
+    'src/main.js'
+];
+
+function build(options=[], env='') {
+    console.log(`building UI for ${env}...`);
+    console.log(`resetting build dir: ${outdir}...`);
+    fs.rmSync(outdir, {recursive: true, force: true});
+    fs.mkdirSync(outdir);
+    console.log(`reset build dir: ${outdir}.`);
+    console.log(`copying index.html to ${outdir}...`);
+    fs.copyFileSync('index.html', `${outdir}/index.html`);
+    console.log(`copied index.html to ${outdir}.`);
+    console.log('building index.js...');
+    let opts = [esbuildBin].concat(options).concat(baseOptions);
+    execSync(opts.join(' '));
+    console.log('built index.js.');
+    console.log(`built UI for ${env}`);
+}
+
+module.exports = build;
