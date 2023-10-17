f7a178a19a8491971902e1afbc7de75cc7117fdf
Chris Reeder <reederc42@gmail.com>
2023-10-17 16:21:41 -0700

    adds test outlines

---
+++ ui/src/store/subjects.test.js
@@ -0,0 +1,8 @@
+import { describe, beforeEach, after, test } from "node:test";
+import assert from "node:assert";
+import { DOM } from "../test-helpers/dom.js";
+
+describe("subjects store", () => {
+    test("update from api");
+    test("")
+});
