ff395ba013421b7dc2135dd79e7eece7647247bd
Chris Reeder <reederc42@gmail.com>
2024-03-08 12:31:54 -0800

    adds working, mostly tested static server

--- wiki/src/spa_server.rs
+++ wiki/src/spa_server.rs
@@ -4,7 +4,7 @@
 // the entry point if the validation passes.
 // TODO: fix up docs.
 
-use std::{collections::HashMap, sync::Arc};
+use std::{collections::HashMap, ffi::OsStr, fs::{self, DirEntry}, io, path::Path, sync::Arc};
 
 use regex::Regex;
 use warp::{
@@ -22,9 +22,10 @@ use warp::{
     Filter,
 };
 
+#[derive(Debug)]
 pub struct Asset {
     pub content_type: String,
-    pub content: String,
+    pub content: Vec<u8>,
 }
 
 pub struct FilterInput {
@@ -42,7 +43,7 @@ pub fn filter(input: Arc<FilterInput>) -> BoxedFilter<(impl Reply,)>
         .boxed()
 }
 
-async fn handler(input: Arc<FilterInput>, path: Tail) -> Result<Response<String>, Rejection>
+async fn handler(input: Arc<FilterInput>, path: Tail) -> Result<Response<Vec<u8>>, Rejection>
 {
     let assets = &input.assets;
     let entrypoint = &input.entrypoint;
@@ -52,6 +53,7 @@ async fn handler(input: Arc<FilterInput>, path: Tail) -> Result<Response<String>
     match assets.get(path) {
         Some(asset) => Ok(found(asset)),
         None => {
+            println!("Testing path: {}", path);
             if path_validator.is_match(path) {
                 Ok(found(assets.get(entrypoint.as_str()).unwrap()))
             } else {
@@ -61,12 +63,71 @@ async fn handler(input: Arc<FilterInput>, path: Tail) -> Result<Response<String>
     }
 }
 
-fn found(asset: &Asset) -> Response<String> {
+fn found(asset: &Asset) -> Response<Vec<u8>> {
     Response::builder()
         .status(StatusCode::OK)
         .header("Content-Type", asset.content_type.clone())
         .body(asset.content.clone())
         .unwrap()
+}
+
+pub fn build_assets(dir: &Path) -> io::Result<HashMap<String, Asset>> {
+    let mut assets = HashMap::new();
+    let content_types = build_content_types_map();
+
+    let mut cb = |entry: &DirEntry| -> io::Result<()> {
+        let content = match fs::read(entry.path()) {
+            Err(e) => return Err(e),
+            Ok(c) => c,
+        };
+
+        let path = entry.path();
+        let ext = path.extension().and_then(OsStr::to_str).unwrap();
+        let content_type = String::from(*content_types.get(ext).unwrap());
+
+        let asset = Asset{content_type, content};
+
+        let asset_path = String::from(
+            entry.path().to_str().unwrap()
+                .strip_prefix(dir.to_str().unwrap()).unwrap()
+                .trim_start_matches('/')
+        );
+
+        assets.insert(asset_path, asset);
+        Ok(())
+    };
+
+    match visit_files(dir, &mut cb) {
+        Err(e) => Err(e),
+        Ok(_) => Ok(assets),
+    }
+}
+
+fn visit_files(dir: &Path, cb: &mut dyn FnMut(&DirEntry) -> io::Result<()>) -> io::Result<()> {
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            if path.is_dir() {
+                visit_files(&path, cb)?;
+            } else {
+                cb(&entry)?;
+            }
+        }
+    }
+    Ok(())
+}
+
+fn build_content_types_map() -> HashMap<&'static str, &'static str> {
+    let mut m = HashMap::new();
+
+    m.insert("ico", "image/x-icon");
+    m.insert("map", "application/json");
+    m.insert("json", "application/json");
+    m.insert("html", "text/html");
+    m.insert("js", "application/javascript");
+
+    m
 }
 
 #[cfg(test)]
@@ -77,7 +138,7 @@ mod tests {
     fn test_found() {
         let asset = Asset {
             content_type: "text/html".to_string(),
-            content: "hello, world".to_string(),
+            content: Vec::from("hello, world"),
         };
 
         let res = found(&asset);
@@ -93,7 +154,7 @@ mod tests {
         let mut assets = HashMap::new();
         assets.insert("index.html".to_string(), Asset{
             content_type: "text/html".to_string(),
-            content: "hello, world!".to_string(),
+            content: Vec::from("hello, world!"),
         });
         let re = Regex::new("wiki").unwrap();
 
@@ -114,7 +175,7 @@ mod tests {
         let mut assets = HashMap::new();
         assets.insert("index.html".to_string(), Asset{
             content_type: "text/html".to_string(),
-            content: "hello, world!".to_string(),
+            content: Vec::from("hello, world!"),
         });
         let re = Regex::new("wiki").unwrap();
 
@@ -127,7 +188,7 @@ mod tests {
 
         let result = handler(input, path).await;
 
-        assert!(result.ok().unwrap().body().contains("hello, world!"));
+        assert_eq!(result.ok().unwrap().body(), &Vec::from("hello, world!"));
     }
 
     #[tokio::test]
@@ -135,7 +196,7 @@ mod tests {
         let mut assets = HashMap::new();
         assets.insert("index.html".to_string(), Asset{
             content_type: "text/html".to_string(),
-            content: "hello, world!".to_string(),
+            content: Vec::from("hello, world!"),
         });
         let re = Regex::new("/wiki").unwrap();
 
@@ -148,7 +209,7 @@ mod tests {
 
         let result = handler(input, path).await;
 
-        assert!(result.ok().unwrap().body().contains("hello, world!"));
+        assert_eq!(result.ok().unwrap().body(), &Vec::from("hello, world!"));
     }
 
     async fn new_tail(path: &str) -> Tail {
