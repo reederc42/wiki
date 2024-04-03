798d47f04f996c1e0f216f98b5d7b986cee298c4
Chris Reeder <reederc42@gmail.com>
2024-04-03 00:11:49 +0000

    adds node checks

--- ci/src/lib.rs
+++ ci/src/lib.rs
@@ -1,4 +1,4 @@
-use std::process::Command;
+use std::{process::Command, rc::Rc};
 
 use clap::Parser;
 
@@ -40,10 +40,16 @@ pub fn cmd(args: Cli) {
         return;
     }
 
-    let context = Context { id: &std::env::var("BUILD_ID").unwrap() };
+    let context = Rc::new(Context {
+        id: std::env::var("BUILD_ID").unwrap_or(String::from("local")),
+        cwd: String::from(std::env::current_dir().unwrap().to_str().unwrap()),
+    });
+    let docker = Rc::new(Docker{
+        context: context.clone(),
+    });
     let config = Config {
-        runner: Box::new(Docker{}),
-        builder: Box::new(Docker{}),
+        runner: docker.clone(),
+        builder: docker.clone(),
     };
 
     for s in all_stages {
@@ -57,13 +63,14 @@ pub fn cmd(args: Cli) {
 
 pub type Error = Result<(), Box<dyn std::error::Error>>;
 
-pub struct Context<'a> {
-    pub id: &'a str,
+pub struct Context {
+    pub id: String,
+    pub cwd: String,
 }
 
 pub struct Config {
-    pub runner: Box<dyn Runner>,
-    pub builder: Box<dyn Builder>,
+    pub runner: Rc<dyn Runner>,
+    pub builder: Rc<dyn Builder>,
 }
 
 pub trait Runner {
@@ -79,7 +86,9 @@ pub trait Stage {
     fn run(&self, context: &Context, config: &Config) -> Error;
 }
 
-struct Docker {}
+struct Docker {
+    context: Rc<Context>,
+}
 
 impl Builder for Docker {
     fn build(&self, tag: &str, dockerfile: &str, context: &str) -> Error {
@@ -105,24 +114,53 @@ impl Builder for Docker {
                                 } else {
                                     Ok(())
                                 }
-                            }
-                            None => {
-                                Err(error!("Process terminated with signal"))
-                            }
+                            },
+                            None => Err(error!("Process terminated with signal")),
                         }
                     },
                     Err(e) => Err(Box::new(e)),
                 }
             },
-            Err(e) => {
-                Err(Box::new(e))
-            },
+            Err(e) => Err(Box::new(e)),
         }
     }
 }
 
 impl Runner for Docker {
-    fn run(&self, _context: &str, _script: &str) -> Error {
-        Ok(())
+    fn run(&self, context: &str, script: &str) -> Error {
+        let cmd = Command::new("docker")
+            .args([
+                "run",
+                "-v",
+                &format!("{}:{}", self.context.cwd, self.context.cwd),
+                "-w",
+                &self.context.cwd,
+                &format!("{}:{}-{}", "wiki-ci", context, self.context.id),
+                "sh",
+                "-c",
+                script,
+            ])
+            .spawn();
+
+        match cmd {
+            Ok(mut c) => {
+                match c.wait() {
+                    Ok(s) => {
+                        match s.code() {
+                            Some(code) => {
+                                if code != 0 {
+                                    Err(error!("Received non-zero exit status: {}", code))
+                                } else {
+                                    Ok(())
+                                }
+                            },
+                            None => Err(error!("Process terminated with signal")),
+                        }
+                    },
+                    Err(e) => Err(Box::new(e)),
+                }
+            },
+            Err(e) => Err(Box::new(e)),
+        }
     }
 }
