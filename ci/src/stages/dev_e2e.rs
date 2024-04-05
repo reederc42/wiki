8246fdbd1504d90a6e7f8ede7b60475560153f42
Chris Reeder <reederc42@gmail.com>
2024-04-05 15:52:16 +0000

    improves dev server startup time

--- ci/src/stages/dev_e2e.rs
+++ ci/src/stages/dev_e2e.rs
@@ -19,38 +19,43 @@ impl Stage for DevE2E {
 
 fn node_dev_e2e(expiration: u32, config: &Config) -> Result<(), Error> {
     let server = config.runner.run_background_server("build", &format!(r"
-            set -xe
-            ln -s /ci/node_modules ./ui/node_modules || true
-            cd ui
-            npm run dev -- --user-expiration {0} --api-expiration {0}
-        ", expiration))?;
+        set -xe
+        ln -s /ci/node_modules ./ui/node_modules || true
+        cd ui
+        npm run dev -- --user-expiration {0} --api-expiration {0}
+    ", expiration))?;
 
-        config.runner.run("e2e", &format!(r"
-            set -xe
-            ln -s /ci/node_modules ./ui/node_modules || true
-            cd ui
-            node tools/configure.js --user-expiration {1} --api-expiration {1}
-            npx cypress run --browser firefox --config baseUrl=http://{0}:8080
-            npx cypress run --browser chrome --config baseUrl=http://{0}:8080
-        ", server.addr(), expiration))
+    config.runner.run("e2e", &format!(r"
+        set -xe
+        ln -s /ci/node_modules ./ui/node_modules || true
+        cd ui
+        node tools/configure.js --user-expiration {1} --api-expiration {1}
+        npx cypress run --browser firefox --config baseUrl=http://{0}:8080
+        npx cypress run --browser chrome --config baseUrl=http://{0}:8080
+    ", server.addr(), expiration))
 }
 
 fn rust_dev_e2e(expiration: u32, config: &Config) -> Result<(), Error> {
-    let server = config.runner.run_background_server("build", &format!(r"
-            set -xe
-            ln -s /ci/node_modules ./ui/node_modules || true
-            cd ui
-            npm run build -- --build dev --user-expiration {0} --api-expiration {0}
-            cd ..
-            cargo run --bin wiki
-        ", expiration))?;
+    config.runner.run("build", &format!(r"
+        set -xe
+        ln -s /ci/node_modules ./ui/node_modules || true
+        cd ui
+        npm run build -- --build dev --user-expiration {0} --api-expiration {0}
+        cd ..
+        cargo build --bin wiki
+    ", expiration))?;
 
-        config.runner.run("e2e", &format!(r"
-            set -xe
-            ln -s /ci/node_modules ./ui/node_modules || true
-            cd ui
-            node tools/configure.js --user-expiration {1} --api-expiration {1}
-            npx cypress run --browser firefox --config baseUrl=http://{0}:8080
-            npx cypress run --browser chrome --config baseUrl=http://{0}:8080
-        ", server.addr(), expiration))
+    let server = config.runner.run_background_server("build", r"
+        set -xe
+        ./target/debug/wiki
+    ")?;
+
+    config.runner.run("e2e", &format!(r"
+        set -xe
+        ln -s /ci/node_modules ./ui/node_modules || true
+        cd ui
+        node tools/configure.js --user-expiration {1} --api-expiration {1}
+        npx cypress run --browser firefox --config baseUrl=http://{0}:8080
+        npx cypress run --browser chrome --config baseUrl=http://{0}:8080
+    ", server.addr(), expiration))
 }
